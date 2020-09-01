70cbdc9c69bdfa4897f55f60fcc6e8e1bb6eac4c
jonathan jorge <jonathanjorge8445@gmail.com>
2020-09-01 19:54:42 -0300

    refactoration of fuction main

--- main.py
+++ main.py
@@ -1,47 +1,69 @@
 import json
 import pandas as pd
+from grafo import *
 
-
-def main():
-    # Abrindo a base de dados
-
+def readDB():
     pd.read_csv("export_gisds.csv").to_json("bd.json")
 
     with open('bd.json') as json_file:
-        data = json.load(json_file)
+        data_set = json.load(json_file)
+    
+    return data_set
 
-    # Montando a lista de adjacencia
+def makeAdjacencyList():
+
+    data_set= readDB()
+
+    # Modelando os dados
+    data = {}
+    cont = 0
+    dictTemp={}
+    title=()
+    for (key, x) in data_set.items():
+
+        title = list(key.split(";"))
+        title = tuple(title[:6])
+        title = tuple(title)
+        for y in x.values():
+            item = y.replace("\"", "").split(";")
+            for group in enumerate(title):
+                data[cont] = {'name': item[group[0]], 'group': group[0]}
+                if not item[group[0]] in dictTemp:
+                    dictTemp[item[group[0]]] = {'id': cont} #enumerando cada nó  
+                cont += 1
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file)
+
+  # Montando a lista de adjacencia
     listaDeAdjacencia = {}
 
-    for (key, x) in data.items():
-        # Separando o Titulo da base de dados
-        title = tuple(key.split(";"))
+    for (key, x) in data_set.items():
+
+        for y in x.values():
+
+            item = y.replace("\"", "").split(";")
+
+            for key in enumerate(title):
+                id_nome = id_vizinho = 0
+                # Criando aresta da especie para a familia
+                if key[0] == 0:
+                    id_nome = dictTemp.get(item[key[0]])['id']
+                    id_vizinho = dictTemp.get(item[5])['id']
+                # Criando aresta para os outros nos de forma decrescente 
+                elif(key[0]+1 < 6):
+                    id_nome = dictTemp.get(item[key[0]+1])['id']
+                    id_vizinho = dictTemp.get(item[key[0]])['id']
+                else:
+                    continue
+                # verificando se o no ja está na lista de adjacencia
+                if not listaDeAdjacencia.get(id_vizinho):
+                    listaDeAdjacencia[id_vizinho] = []
+                # verificando se a aresta ja foi colocada
+                if not id_nome in listaDeAdjacencia[id_vizinho]:
+                    listaDeAdjacencia[id_vizinho].append(id_nome)
         
-        for (index, value) in enumerate(title):
-
-            if(index != 0 and (index < len(title)-2)):
-                
-                for z in x.values():
-                    
-                    item = z.split(";")
-
-                    name = item[index]
-
-                    if not listaDeAdjacencia.get(name):
-                        listaDeAdjacencia[name] = []
-
-                    if not item[index+1] in listaDeAdjacencia[name]:
-                        if index+1 == 6:
-                            listaDeAdjacencia[name].append(item[0])
-                        else:
-                            listaDeAdjacencia[name].append(item[index+1])
-
-        print(listaDeAdjacencia)
-
-        # Salvando lista de adjacência
-        with open('listaAdjacência.json', 'w') as json_file:
-            json.dump(listaDeAdjacencia, json_file)
+        return (listaDeAdjacencia)
 
 
 if __name__ == "__main__":
-    main()
+   makeAdjacencyList()
