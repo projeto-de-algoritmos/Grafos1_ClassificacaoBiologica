41d2e6ef4260dc09783c5981bc8f230a77ac0c7a
Filipecgmr <51974679+Filipecgmr@users.noreply.github.com>
2020-09-06 12:32:19 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import os
 # from main import *
 # Ler 'banco de dados' e pre formata para uso
 
@@ -9,44 +10,59 @@ def read_db():
     data_set = data_set.iloc[:, 0:6].to_dict()
     return data_set
 
-# retorna id ou nome da lista
-
-
-def get_id_name(id=None, name=None):
-    data = enumerate_data()
-    if id:
-        return data[id]
-    if name:
-        return list(data.values()).index(name)
-
-# Enumera cada nome da taxonomia
-
+data_enumareted = {}
 
 def enumerate_data():
+    global data_enumareted
     data_set = read_db()
     cont = 1
-    data_enumareted = {}
     for x in range(len(data_set['Species'])):
         for groups in ('Species', 'Family', 'Order', 'Class', 'Phylum', 'Kingdom'):
-            data_enumareted[cont] = data_set[groups][x]
-            cont += 1
+            if (data_set[groups][x] not in list(data_enumareted.values())):
+                data_enumareted[cont] = data_set[groups][x]
+                cont += 1
 
     with open('bd.json', 'w') as json_file:
         json.dump(data_enumareted, json_file)
 
     return data_enumareted
 
+data_enumareted = enumerate_data()
 
+# retorna id ou nome da lista
+def get_id_name(id=None, name=None):
+    global data_enumareted
+    data = data_enumareted
+    if id:
+        return data[id]
+    if name:
+        return list(data.values()).index(name)+1
+
+def read_adjacent_list():
+    if not os.path.exists('data.json'):
+        make_adjacent_list()
+    with open('listaAjacencia.json', 'r') as json_file:
+        adjacency_list = json.load(json_file)
+    return (adjacency_list)
+
+# Enumera cada nome da taxonomia
 def make_adjacent_list():
+    enumerate_data()
     adjacency_list = {}
     groups = {0: 'Species', 1: 'Family', 2: 'Order',
               3: 'Class', 4: 'Phylum', 5: 'Kingdom'}
+
     data = read_db()
+
     for row_in_data in range(len(data['Species'])):
         for (key, name) in groups.items():
+
             # Adicionando Reinos à raiz
-            # if not (get_id_name(data['Kindgom'][row_in_data] in adjacency_list.get(0))):
-            #     adjacency_list[0].append(get_id_name(name=data['Kindgom'][row_in_data]))
+            if not adjacency_list.get(0):
+                adjacency_list[0] = []
+            if not (get_id_name(name=data['Kingdom'][row_in_data]) in adjacency_list.get(0)):
+                adjacency_list[0].append(get_id_name(
+                    name=data['Kingdom'][row_in_data]))
 
             if(key < len(groups)-1):
                 # Pegando id dos nós
@@ -69,10 +85,10 @@ if __name__ == "__main__":
     with open('listaAjacencia.json', 'r') as json_file:
         adjacency_list = json.load(json_file)
 
-    a= find_path(adjacency_list, 77, get_id_name(name="Suncus murinus"))
+    a = find_path(adjacency_list, 77, get_id_name(name="Suncus murinus"))
     for x in a:
         print(get_id_name(id=x))
- 
+
 
 def find_path(graph, start, end, path=[]):
     path = path + [start]
@@ -86,6 +102,20 @@ def find_path(graph, start, end, path=[]):
             if newpath: return newpath
     return None
 
+
+def search():
+    adjacency_list = read_adjacent_list()
+    i = input("please type what you want to search for:\n").lower().capitalize()
+    j = get_id_name(name=i)
+    path = find_path(adjacency_list, 0, j)
+    if (path == None):
+        print("\n\n\n-----------------------------\n Taxonomia nao encontrada\n-----------------------------\n\n\n")
+    else:
+        for x in path:
+            if x != 0:
+                print(get_id_name(id=x))
+
+
 def Menu_de_cadastro():
     while 1:
         print('''Do you want to register by:\n
@@ -96,61 +126,61 @@ def Menu_de_cadastro():
 [5] species\n''')
 
         op = input("Please enter the number of the option you want to select:")
-        
+
         if op == "1":
             os.system('clear')
-            ajuda = input("Enter the Kingdom to which this Phylum belongs:")
-            #funçao de busca recebendo o nó Busca_cadastro(ajuda)
+            help = input("Enter the Kingdom to which this Phylum belongs:")
+            # funçao de busca recebendo o nó Busca_cadastro(help)
             pass
-                
+
         elif op == "2":
             os.system('clear')
-            ajuda = input("Enter the Philo that this Class belongs to:") 
-            #funçao de busca recebendo o nó Busca_cadastro(ajuda)       
-            pass  
+            help = input("Enter the Philo that this Class belongs to:")
+            # funçao de busca recebendo o nó Busca_cadastro(help)
+            pass
 
         elif op == "3":
             os.system('clear')
-            ajuda = input("Enter the Class to which this Order belongs:") 
-            #funçao de busca recebendo o nó Busca_cadastro(ajuda)        
-            pass 
+            help = input("Enter the Class to which this Order belongs:")
+            # funçao de busca recebendo o nó Busca_cadastro(help)
+            pass
 
         elif op == "4":
-            os.system('clear') 
-            ajuda = input("Enter the Order to which this Family belongs:")  
-            #funçao de busca recebendo o nó Busca_cadastro(ajuda)               
-            pass 
+            os.system('clear')
+            help = input("Enter the Order to which this Family belongs:")
+            # funçao de busca recebendo o nó Busca_cadastro(help)
+            pass
 
         elif op == "5":
-            os.system('clear') 
-            ajuda = input("Enter the Family to which this species belongs:")   
-            #funçao de busca recebendo o nó Busca_cadastro(ajuda)                 
-            pass 
-                    
+            os.system('clear')
+            help = input("Enter the Family to which this species belongs:")
+            # funçao de busca recebendo o nó Busca_cadastro(help)
+            pass
+
         else:
-            os.system('clear') 
+            os.system('clear')
             print("\n\n\n---------------Please enter a valid option---------------\n\n\n")
 
-def Menuinicial(graph):
-    os.system('clear') 
-    
+
+def Menuinicial():
+    os.system('clear')
     while 1:
         print('''Home menu:\n
 [1] Search
 [2] register\n ''')
 
         op = input("Please enter the number of the option you want to select:")
-    
+
         if op == "1":
             os.system('clear')
-            search(graph)
+            search()
             pass
-                
+
         elif op == "2":
-            os.system('clear')         
+            os.system('clear')
             Menu_de_cadastro()
-            pass  
-                    
+            pass
+
         else:
-            os.system('clear') 
+            os.system('clear')
             print("\n\n\n---------------Please enter a valid option---------------\n\n\n")
