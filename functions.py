b5d0996447e5790974c7f5b8175338ddc6383471
jonathan jorge <jonathanjorge8445@gmail.com>
2020-09-07 18:02:58 -0300

    final part

--- functions.py
+++ functions.py
@@ -4,8 +4,7 @@ import os
 # from main import *
 
 # Ler 'banco de dados' e pre formata para uso
-columns = ['species', 'genus', 'family',
-           'order', 'class', 'phylum', 'kingdom']
+data_enumerated = {}
 
 
 def read_db():
@@ -34,58 +33,72 @@ def write_db(taxonomic=[]):
     return df
 
 
-data_enumareted = {}
-
 # Enumera cada nome da taxonomia
 
 
 def enumerate_data():
-    global data_enumareted
+    global data_enumerated
     data_set = read_db()
     cont = 1
     for x in range(len(data_set['species'])):
         for groups in ('species', 'genus', 'family', 'order', 'class', 'phylum', 'kingdom'):
-            if (data_set[groups][x] not in list(data_enumareted.values())):
-                data_enumareted[cont] = data_set[groups][x]
+            if (data_set[groups][x] not in list(data_enumerated.values())):
+                data_enumerated[cont] = data_set[groups][x]
                 cont += 1
 
-    with open('bd.json', 'w') as json_file:
-        json.dump(data_enumareted, json_file)
-    return data_enumareted
+    with open('de.json', 'w') as json_file:
+        json.dump(data_enumerated, json_file)
+    return data_enumerated
 
 
-data_enumareted = enumerate_data()
+data_enumerated = enumerate_data()
+
 
 # retorna id ou nome da lista
-
-
 def get_id_name(id=None, name=None):
-    global data_enumareted
-    data = data_enumareted
+    global data_enumerated
+    data = data_enumerated
     if id:
         if not id in list(data.keys()):
-            return -1
+            if not str(id) in list(data.keys()):
+                return -1
         return data[id]
     if name:
         if not name in list(data.values()):
             return -1
-        return list(data.values()).index(name)+1
+        return str(list(data.values()).index(name)+1)
+
+
+def enumerate_new_data(data_enumerated, species):
+    cont = len(data_enumerated)+1
+    for x in species:
+        if-1 == get_id_name(name=x):
+            data_enumerated[str(cont)] = x
+            cont += 1
+
+    return data_enumerated
+
+
+def read_enumerated_data():
+    if not os.path.exists('de.json'):
+        enumerate_data()
+    with open('de.json', 'r') as json_file:
+        data_enumerated = json.load(json_file)
+    return (data_enumerated)
+
 
 # ler a lista de adjacencia do arquivo json caso ela nao exista
-
-
 def read_adjacent_list():
     if not os.path.exists('listaAjacencia.json'):
         make_adjacent_list()
     with open('listaAjacencia.json', 'r') as json_file:
         adjacency_list = json.load(json_file)
+
     return (adjacency_list)
 
 
 # Constroi a lisa de adjacencia
 def make_adjacent_list():
-    global data_enumareted
-    data_enumareted = enumerate_data()
     adjacency_list = {}
     groups = {0: 'species', 1: 'genus', 2: 'family', 3: 'order',
               4: 'class', 5: 'phylum', 6: 'kingdom'}
@@ -119,11 +132,10 @@ def insert_in_adjacent_list(adjacency_list, species):
                 adjacency_list[id_neighbor].append(id_no)
         elif index == len(species):
             # Adicionando Reinos à raiz
-            if not adjacency_list.get(0):
-                adjacency_list[0] = []
-            if not (get_id_name(name=species[-1]) in adjacency_list.get(0)):
-                adjacency_list[0].append(get_id_name(
-                    name=species[-1]))
+            if not adjacency_list.get('0'):
+                adjacency_list['0'] = []
+            if not (get_id_name(name=species[-1]) in adjacency_list.get('0')):
+                adjacency_list['0'].append(get_id_name(name=species[-1]))
 
     return adjacency_list
 
@@ -232,13 +244,19 @@ def Menu_de_cadastro():
 # Registra a especie a partir do ultimo nó em comum existente
 def register_from_this(register, rest):
     rest.pop()
+    global data_enumerated
     while rest:
         if get_id_name(id=rest[-1]):
             register.append(get_id_name(id=rest.pop()))
         else:
             rest.pop()
     write_db(register)
-    enumerate_data()
+    data_enumerated = read_enumerated_data()
+    data_enumerated = enumerate_new_data(data_enumerated, register)
+
+    with open('de.json', 'w') as json_file:
+        json.dump(data_enumerated, json_file)
+
     adjacency_list = read_adjacent_list()
     adjacency_list = insert_in_adjacent_list(adjacency_list, register)
 
